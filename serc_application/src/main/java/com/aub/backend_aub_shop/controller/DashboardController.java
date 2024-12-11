6276450668855a54f8e918349b27e0d1a99f74d5
torngzz <chukizz904@gmail.com>
2024-12-11 09:53:04 +0700

    add .

--- serc_application/src/main/java/com/aub/backend_aub_shop/controller/DashboardController.java
+++ serc_application/src/main/java/com/aub/backend_aub_shop/controller/DashboardController.java
@@ -10,19 +10,18 @@ import com.aub.backend_aub_shop.service.DashboardService;
 import com.aub.backend_aub_shop.service.UserService;
 
 @Controller
-@RequestMapping(value = {"/dashboard"})
+@RequestMapping(value = {"", "/dashboard"})
 public class DashboardController {
 
   @Autowired DashboardService dashboardService;
   @Autowired UserService userService;
 
-
-    @GetMapping("/")
-    public String showDashboard(Model model){
-      int totalUsers = userService.CountTotalUser();
-      model.addAttribute("totalUsers", totalUsers);
-      return "/Dashboard/dashboard";
-    }
+  @GetMapping(value = {"", "/"})
+  public String showDashboard(Model model){
+    int totalUsers = userService.CountTotalUser();
+    model.addAttribute("totalUsers", totalUsers);
+    return "/Dashboard/dashboard";
+  }
 
     // @GetMapping("/getTotalUser")
     // public DashboardModel  getTotalUser(){
